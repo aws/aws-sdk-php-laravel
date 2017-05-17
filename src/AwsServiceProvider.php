a15cc5f188e1266a048a7ff4878355074db5155f
Chris Morrell <inxilpro@users.noreply.github.com>
2017-05-17 12:23:37 -0400

    Lumen compatibility

--- src/AwsServiceProvider.php
+++ src/AwsServiceProvider.php
@@ -5,7 +5,6 @@ use Aws\DynamoDb\SessionHandler;
 use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Session;
 use Laravel\Lumen\Application as LumenApplication;
 
 /**
@@ -32,21 +31,23 @@ class AwsServiceProvider extends ServiceProvider
 
         $this->mergeConfigFrom($source, 'aws');
 
-        Session::extend('dynamodb', function ($app) {
-            $client = $app->make('aws')->createClient('DynamoDb');
-            $config = $app->make('config')->get('session');
-            return SessionHandler::fromClient($client, array_filter([
-                'table_name' => Arr::get($config, 'table'),
-                'hash_key' => Arr::get($config, 'hash_key'),
-                'session_lifetime' => Arr::get($config, 'lifetime'),
-                'consistent_read' => Arr::get($config, 'consistent_read'),
-                'batch_config' => Arr::get($config, 'batch_config'),
-                'locking' => Arr::get($config, 'locking'),
-                'max_lock_wait_time' => Arr::get($config, 'max_lock_wait_time'),
-                'min_lock_retry_microtime' => Arr::get($config, 'min_lock_retry_microtime'),
-                'max_lock_retry_microtime' => Arr::get($config, 'max_lock_retry_microtime'),
-            ]));
-        });
+        if ($this->app->bound('session')) {
+            $this->app->make('session')->extend('dynamodb', function ($app) {
+                $client = $app->make('aws')->createClient('DynamoDb');
+                $config = $app->make('config')->get('session');
+                return SessionHandler::fromClient($client, array_filter([
+                    'table_name' => Arr::get($config, 'table'),
+                    'hash_key' => Arr::get($config, 'hash_key'),
+                    'session_lifetime' => Arr::get($config, 'lifetime'),
+                    'consistent_read' => Arr::get($config, 'consistent_read'),
+                    'batch_config' => Arr::get($config, 'batch_config'),
+                    'locking' => Arr::get($config, 'locking'),
+                    'max_lock_wait_time' => Arr::get($config, 'max_lock_wait_time'),
+                    'min_lock_retry_microtime' => Arr::get($config, 'min_lock_retry_microtime'),
+                    'max_lock_retry_microtime' => Arr::get($config, 'max_lock_retry_microtime'),
+                ]));
+            });
+        }
     }
 
     /**
