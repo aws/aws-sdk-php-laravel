927632614044d30f0368bb1eab2937c97da711df
Howard Lopez <lopehowa@amazon.com>
2018-11-27 15:51:34 -0800

    Modify service provider to use different config for publishing and default usage

--- config/aws_default.php
+++ config/aws_default.php
@@ -10,16 +10,13 @@ return [
     |--------------------------------------------------------------------------
     |
     | The configuration options set in this file will be passed directly to the
-    | `Aws\Sdk` object, from which all client objects are created. The minimum
-    | required options are declared here, but the full set of possible options
+    | `Aws\Sdk` object, from which all client objects are created. This file
+    | sets the default minimum configuration used by the service provider even
+    | if no configuration is set by the user. The full set of possible options
     | are documented at:
     | http://docs.aws.amazon.com/aws-sdk-php/v3/guide/guide/configuration.html
     |
     */
-    'credentials' => [
-        'key'    => env('AWS_ACCESS_KEY_ID', ''),
-        'secret' => env('AWS_SECRET_ACCESS_KEY', ''),
-    ],
     'region' => env('AWS_REGION', 'us-east-1'),
     'version' => 'latest',
     'ua_append' => [
