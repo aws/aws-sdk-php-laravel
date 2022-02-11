257b3c11deae64aad24210d894f7e8d3c8258af2
Victor Tang <tanghengzhi@live.com>
2022-02-11 18:55:21 +0800

    phpunit

--- tests/AwsServiceProviderTest.php
+++ tests/AwsServiceProviderTest.php
@@ -53,7 +53,7 @@ abstract class AwsServiceProviderTest extends TestCase
         $config = $app['config']->get('aws');
 
         $this->assertArrayHasKey('ua_append', $config);
-        $this->assertInternalType('array', $config['ua_append']);
+        $this->assertIsArray($config['ua_append']);
         $this->assertNotEmpty($config['ua_append']);
         $this->assertNotEmpty(array_filter($config['ua_append'], function ($ua) {
             return false !== strpos($ua, AwsServiceProvider::VERSION);
