a61fc41970a8a9b176c9ab9a80930702b9b339c8
Sean O'Brien <obrien.sean.dev@gmail.com>
2023-02-15 20:27:51 -0500

    update CI to run tests against PHP 8-8.2

--- tests/LumenAwsServiceProviderTest.php
+++ tests/LumenAwsServiceProviderTest.php
@@ -6,17 +6,11 @@ use Laravel\Lumen\Application;
 
 class LumenAwsServiceProviderTest extends AwsServiceProviderTest
 {
-    public function setUp()
+    protected function setupApplication()
     {
         if (!class_exists(Application::class)) {
             $this->markTestSkipped();
         }
-
-        parent::setUp();
-    }
-
-    protected function setupApplication()
-    {
         // Create the application such that the config is loaded.
         $app = new Application(sys_get_temp_dir());
         $app->instance('config', new Repository());
